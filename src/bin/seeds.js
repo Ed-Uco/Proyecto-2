fdb2df5f83803622e5e78236935f625b316c9e7a
KuraiTsukino <sandym3000@hotmail.com>
2021-11-29 12:02:04 -0600

    modelo de Movie

--- src/bin/seeds.js
+++ src/bin/seeds.js
@@ -7,3 +7,11 @@ require("dotenv").config()
 
 // 2. Conexión a base de datos
 
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.hptoy.mongodb.net/marvel`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+// 3. Datos a poblar.
+
+const movies
