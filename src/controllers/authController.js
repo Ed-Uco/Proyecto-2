a35ce63dae4792062035fb3a56f2a856470d392f
Edgar Uco <edgarin@gmail.com>
2021-11-30 11:37:31 -0600

    login signup access added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,116 +1,110 @@
-// ./controllers/authController.js
-const User = require('./../models/User');
+// Import
 const bcryptjs = require('bcryptjs');
-exports.viewRegister = (req, res) => {
+const User = require('./../models/User');
+const mongoose = require('mongoose');
+// Functions Signup
+exports.getSignup = (req, res) => {
     res.render('auth/signup');
 };
-exports.register = async (req, res) => {
-    //1. OBTENCION DE DATOS DEL FORMULARIOS
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-      console.log(username, email, password);
-    //A) VALIDACION --VERIFICACION DE CAMPOS VACIOS.
-    //VERIFICAR QUE USERNAME, EMAIL Y PASSWORD TENGAN CONTENIDO, ES DECIR QUE NO LLEGUEN VACIOS.
-    if (!username || !email || !password) {
-        res.render('auth/signup', {
-            errorMessage:
-                'Uno o más campos están vacíos. Revísalos nuevamente.',
-        });
-        return;
-    }
 
-    // B) VALIDACION - FORTALECIMIENTO DE PASSWORD.
-    // VERIFIQUE EL PASSWORD TENGA 6 CARACTERES, MINIMO UN NUMERO Y UNA MAYUSCULA.
-    //REGEX - CONJUNTO DE REGLAS QUE AUDITAN UN TEXTO PLANO
+exports.postSignup = async (req, res) => {
+    // Get Data
+    const { name, email, password } = req.body;
+    // Validations
+    if ((!name, !email, !password)) {
+        return res.render('auth/signup', {
+            msg: 'All fields required.',
+        });
+    }
+    // Regex
     const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-
     if (!regex.test(password)) {
-        res.render('auth/signup', {
-            errorMessage:
-                'Tu password debe de contener 6 caracteres, mínimo un número y una mayúscula',
+        return res.render('auth/signup', {
+            msg: 'Please include 6 characters, 1 number, 1 uppercase, 1 lowercase',
         });
-        return;
     }
-
-    // 2. ENCRIPTACIÓN DE PASSWORD 🚩🚩🚩 IMPORTANTE IMPLEMENTARLO
+    // Create user
     try {
+        // Encript password
+        // Times of Hashing
         const salt = await bcryptjs.genSalt(10);
-        const passwordEncriptado = await bcryptjs.hash(password, salt);
-        const newUser = await User.create({
-            username,
+        // Passoword Hashed
+        const hashed = await bcryptjs.hash(password, salt);
+
+        // Create user
+        const createdUser = await User.create({
+            name,
             email,
-            passwordEncriptado,
+            password: hashed,
+            imgUrl: 'https://library.kissclipart.com/20180906/wtq/kissclipart-user-profile-clipart-user-profile-computer-icons-15b5c3086edf7512.png',
         });
-
-        console.log(newUser);
-
-        //3 .REDIRECCION DE USUARIOS
-        res.redirect('/auth/login');
+        // Create session
+        req.session.currentUser = {
+            _id: createdUser._id,
+            name: createdUser.name,
+            email: createdUser.email,
+            imgUrl: createdUser.imgUrl,
+        };
+        // Redirection
+        res.redirect(`/user/${createdUser.name}`);
     } catch (error) {
-        console.log(error);
-        res.status(500).render('auth/signup', {
-            errorMessage:
-                'Hubo un error con la validez de tu correo. Intenta nuevamente. No dejes espacios y usa minúsculas.',
-        });
+        // Validar email desde servidor
+        if (error instanceof mongoose.Error.ValidationError) {
+            res.render('auth/signup', {
+                msg: 'Use a valid email',
+            });
+        } else if (error.code === 11000) {
+            res.render('auth/signup', {
+                msg: 'Email already exist. Try another.',
+            });
+        }
     }
 };
-
-exports.viewLogin = async (req, res) => {
+// Functions Login
+exports.getLogin = (req, res) => {
     res.render('auth/login');
 };
 
-exports.login = async (req, res) => {
+exports.postLogin = async (req, res) => {
+    // Get Data
+    const { email, password } = req.body;
+
+    // Find User
     try {
-        // 1. OBTENCIÓN DE DATOS DEL FORMULARIO
-        const email = req.body.email;
-        const password = req.body.password;
-
-        // 2. VALIDACIÓN DE USUARIO ENCONTRADO EN BD
-        const foundUser = await User.findOne({ email });
-        if (!foundUser) {
-            res.render('auth/login', {
-                errorMessage: 'Email o contraseña sin coincidencia',
+        const findUser = await User.findOne({ email });
+        if (!findUser) {
+            return res.render('auth/login', {
+                msg: 'User not Found',
             });
-            return;
         }
-        // 3. VALIDACIÓN DE CONTRASEÑA
-        // COMPARAR LA CONTRASEÑA DEL FORMULARIO (1) VS LA CONTRASEÑA DE LA BASE DE DATOS (2)
-
-        const verifiedPass = await bcryptjs.compareSync(
+        // Check Password - return boolean
+        const checkedPassword = await bcryptjs.compareSync(
             password,
-            foundUser.passwordEncriptado,
+            findUser.password,
         );
 
-        if (!verifiedPass) {
-            res.render('auth/login', {
-                errorMessage: 'Email o contraseña errónea. Intenta nuevamente',
+        if (!checkedPassword) {
+            return res.render('auth/login', {
+                msg: 'Invalid Password',
             });
-            return;
         }
-
-        // 4. (PRÓXIMAMENTE) GENERAR LA SESIÓN
-        //PERSISTENCIA DE IDENTIDAD
         req.session.currentUser = {
-            _id: foundUser._id,
-            username: foundUser.username,
-            email: foundUser.email,
-            mensaje: 'LO LOGRAMOS!',
+            _id: findUser._id,
+            name: findUser.name,
+            email: findUser.email,
+            imgUrl: findUser.imgUrl,
         };
-        // 5. REDIRECCIONAR AL HOME
-        res.redirect('/users/profile');
-    } catch (error) {
-        console.log(error);
+        // Redirect
+        res.redirect(`/user/${findUser.name}`);
+    } catch (e) {
+        console.log(e);
     }
 };
 
-exports.logout = async (req, res) => {
-    req.session.destroy(error => {
-        //SE EVALUA SI HUBO UN ERROR AL BORRAR LA COOKIE
-        if (error) {
-            console.log(error);
-            return;
-        }
-        res.redirect('/');
-    });
+// Function Logout
+exports.postLogout = async (req, res) => {
+    res.clearCookie('session-token');
+    req.session.destroy(err =>
+        err ? console.log(e) : res.redirect('/auth/login'),
+    );
 };
--- src/middlewares/route-guard.js
+++ src/middlewares/route-guard.js
@@ -1,30 +1,7 @@
-//./middlewares/route-guard.js
-//AREAS PRIVADAS - EL USUARIO DEBE DE ESTAR LOGGEADO PARA ACCEDER
+const isLoggedIn = (req, res, next) =>
+    req.session.currentUser ? next() : res.redirect('/auth/login');
 
-const usuarioLoggeado = (req, res, next) => {
-    // EVALUAR SI EL USUARIO NO ESTÁ LOGGEADO
-    // SI NO ESTÁ LOGGEADO ENVIARLO A LOGIN...
-    if (!req.session.currentUser) {
-        res.redirect('/auth/login');
-        return;
-    }
-    // SI SÍ ESTÁ LOGGEADO ENVIARLO A LA SIGUIENTE FUNCIÓN (CONTROLLER)
-    next();
-};
+const isLoggedOut = (req, res, next) =>
+    req.session.currentUser ? res.redirect('/') : next();
 
-//AREAS DE AUTENTICACION- EL USUARIO YA SE AUTENTICÓ Y QUIERE ENTRAR A LAS ÁREAS DE SIGUP Y LOGIN. POR LO TANTO LO REDIRIGIMOS AL HOME.
-const usuarioNoLoggeado = (req, res, next) => {
-    //EVALUAR SI ESTÀ AUTENTICADO
-    // SI SÍ ESTÁ AUTENTICADO
-    if (req.session.currentUser) {
-        return res.redirect('/');
-    }
-
-    // SI NO ESTÁ AUTENTICADO, DÉJALO PASAR AL SIGNUP O LOGIN
-    next();
-};
-
-module.exports = {
-    usuarioLoggeado,
-    usuarioNoLoggeado,
-};
+module.exports = { isLoggedIn, isLoggedOut };
