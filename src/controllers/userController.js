778931bcd23fa1a4bb47dfc7e432b6e32ef23e99
Edgar Uco <edgarin@gmail.com>
2021-12-01 12:47:29 -0600

    edit user function added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,19 @@
-exports.getProfile = (req, res) => {
-    res.render('user/profile');
+const User = require('./../models/User');
+
+exports.getProfile = async (req, res) => {
+    const username = req.params.user;
+    console.log(username);
+    const getUserData = await User.findOne({ name: username });
+    res.render('user/profile', {
+        data: getUserData,
+    });
 };
-const User = require('./../models/User')
 
 exports.viewEditUser = async (req, res) => {
-    console.log(req.params);
+    console.log('este es el req.params ´viewEditUser', req.params);
 
     const userID = req.params.userID;
-
+    console.log(userID);
     const foundUser = await User.findById(userID);
 
     res.render('user/editUser', {
@@ -17,11 +23,13 @@ exports.viewEditUser = async (req, res) => {
 
 exports.editUser = async (req, res) => {
     // 1. EL ID DEL LIBRO
+    console.log('este es el req.params EditUser', req.params);
     const userID = req.params.userID;
 
     // 2. LOS NUEVOS CAMBIOS DEL FORMULARIO
     const name = req.body.name;
     const email = req.body.email;
+    const imgUrl = req.body.imgUrl;
 
     console.log(userID);
     console.log(name, email);
@@ -30,11 +38,16 @@ exports.editUser = async (req, res) => {
     // findByIdAndUpdate([ID], [NUEVOS CAMBIOS EN OBJETO], [DEVOLVER A LA VARIABLE LA ACTUALIZACIÓN])
     const updatedUser = await User.findByIdAndUpdate(
         userID, // ID DEL DOCUMENTO
-        { name, email },
+        { name, email, imgUrl },
         { new: true }, // DEVOLVER A LA VARIABLE EL DOCUMENTO ACTUALIZADO
     );
 
     console.log(updatedUser);
-
-    res.redirect(`/user/${updatedUser._id}`);
+    req.session.currentUser = {
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        imgUrl: updatedUser.imgUrl,
+    };
+    res.redirect(`/user/${updatedUser.name}`);
 };
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -5,12 +5,13 @@ const { getProfile } = require('./../controllers/userController');
 const { isLoggedIn } = require('./../middlewares/route-guard');
 const userController =require('./../controllers/userController')
 
-
+router.get('/:userID/editUser', userController.viewEditUser);
+router.post('/:userID/editUSer', userController.editUser);
 
 // Routes
 router.get('/:user', isLoggedIn, getProfile);
 // EDIT
-router.get('/:user', userController.viewEditUser);
-router.post('/:user', userController.editUser);
+/* router.get('/:user', userController.viewEditUser);
+router.post('/:user', userController.editUser); */
 // Export
 module.exports = router;
