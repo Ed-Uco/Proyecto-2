089bde111eee28a3d692c5011f01d8def87007c9
Edgar Uco <edgarin@gmail.com>
2021-11-30 20:21:30 -0600

    function edit profile added partially

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,40 @@
 exports.getProfile = (req, res) => {
     res.render('user/profile');
 };
+const User = require('./../models/User')
+
+exports.viewEditUser = async (req, res) => {
+    console.log(req.params);
+
+    const userID = req.params.userID;
+
+    const foundUser = await User.findById(userID);
+
+    res.render('user/editUser', {
+        data: foundUser,
+    });
+};
+
+exports.editUser = async (req, res) => {
+    // 1. EL ID DEL LIBRO
+    const userID = req.params.userID;
+
+    // 2. LOS NUEVOS CAMBIOS DEL FORMULARIO
+    const name = req.body.name;
+    const email = req.body.email;
+
+    console.log(userID);
+    console.log(name, email);
+
+    // 3. REALIZAR LA ACTUALIZACIÓN DE DATOS EN LA BASE DE DATOS
+    // findByIdAndUpdate([ID], [NUEVOS CAMBIOS EN OBJETO], [DEVOLVER A LA VARIABLE LA ACTUALIZACIÓN])
+    const updatedUser = await User.findByIdAndUpdate(
+        userID, // ID DEL DOCUMENTO
+        { name, email },
+        { new: true }, // DEVOLVER A LA VARIABLE EL DOCUMENTO ACTUALIZADO
+    );
+
+    console.log(updatedUser);
+
+    res.redirect(`/user/${updatedUser._id}`);
+};
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -3,6 +3,13 @@ const router = require('express').Router();
 const { getProfile } = require('./../controllers/userController');
 
 const { isLoggedIn } = require('./../middlewares/route-guard');
+const userController =require('./../controllers/userController')
+
+// EDIT
+/* router.get('/:userID/edit', userController.viewEditUser);
+router.post('/:userID/edit', userController.editUser); */
+router.get('/:user', userController.viewEditUser);
+router.post('/:user', userController.editUser);
 
 // Routes
 router.get('/:user', isLoggedIn, getProfile);
